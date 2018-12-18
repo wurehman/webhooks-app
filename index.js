03dffe4ca75eab608a811b06998e64b2caf7a935
rehmaw <Waqas.Rehman@pointclickcare.com>
2018-12-18 17:29:32 -0500

    Listen on port 80 instead of secure port

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
 const bodyParser = require('body-parser');
 
 var app = express();
-
 var data;
 
-var options = {
-    key: fs.readFileSync('certs/server.key'),
-    cert: fs.readFileSync('certs/server.crt')
-}
-
-app.use(bodyParser.urlencoded({
-    extended: true
+app.use(bodyParser.urlencoded({ 
+    extended: false 
 }));
 
 app.use(bodyParser.json());
 
-app.post('/webhooks', (req, res) => {
+app.get('/webhooks', (req,res) => {
+    
+    console.log('*************');
+    console.log('GET /webhooks');
+    res.json(data);  
+});
+
+app.post('/webhooks', (req,res) => {
     console.log('*************');
     console.log('POST /webhooks');
     data= req.body;
@@ -27,15 +26,9 @@ app.post('/webhooks', (req, res) => {
     res.end('Ok');
 });
 
-app.get('/webhooks', (req, res) => {
-    console.log('*************');
-    console.log('GET /webhooks');
-    res.json(data);    
-})
+const port=process.env.PORT || 80;
 
-const port=process.env.PORT || 443;
-
-https.createServer(options, app).listen(port, () => {
-  console.log('Webhooks app listening on port ' + port)
-})
+app.listen(port, () => {
+    console.log('Webhooks app listening on port ' + port)
+});
 
